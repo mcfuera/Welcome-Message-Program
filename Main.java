14fbd6a22d88c9c4c5ffd0bc43d1fbcb2df39655
mcfuera <144236384+mcfuera@users.noreply.github.com>
2023-09-26 21:39:08 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Welcome To Java");
+    System.out.println("Welcome to Computer Science");
+    System.out.println("Programming is fun");
+  }
+}
